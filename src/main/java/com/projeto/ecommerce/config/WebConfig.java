4bb7e6c2eead9e5ff4e0f50ea9501fe259d1273a
Cristiano G. <cristianog.contato@gmail.com>
2025-08-19 13:45:50 -0300

    refactor: adicionar url site em cors

--- src/main/java/com/projeto/ecommerce/config/WebConfig.java
+++ src/main/java/com/projeto/ecommerce/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig {
                 registry.addMapping("/**")
                         .allowedOrigins(
                                 "http://localhost:5173",
-                                "https://api.cristianogoncalves.tech"
+                                "https://loja.cristianog.site"
                         )
 
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedHeaders("*")
